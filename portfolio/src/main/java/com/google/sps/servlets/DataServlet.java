7a843517500518d104242fde8bdbe5d7ef66c33c
Yew Onn <khaw.yewonn@gmail.com>
2020-07-17 19:14:17 +0800

    Fix error related to git rebase

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,17 +14,13 @@
 
 package com.google.sps.servlets;
 import com.google.gson.Gson;
-<<<<<<< HEAD
 import com.google.sps.servlets.Constants;
-=======
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-import java.io.*;
->>>>>>> Add persistent comment functionality
 import java.util.ArrayList;
 import java.io.IOException;
 import java.util.*;
